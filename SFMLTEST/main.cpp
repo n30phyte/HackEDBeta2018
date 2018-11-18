b45beb624398c81f1a5470a186e8f2cf75b1341a
shadow <shadowcharmeleon@gmail.com>
2018-11-17 20:02:26 -0700

    HERES YOUR ARIAL REEEE

--- SFMLTEST/main.cpp
+++ SFMLTEST/main.cpp
@@ -2,6 +2,71 @@
 #include <iostream>
 #include "Menu.h"
 
+void pong()
+{
+    sf::RenderWindow window(sf::VideoMode(160, 160, 8), "pong", sf::Style::Default);
+    sf::Vector2f paddleSize(4, 16);
+    sf::Vector2f ballSize(2, 4);
+
+    sf::RectangleShape leftPaddle;
+    leftPaddle.setSize(paddleSize);
+    leftPaddle.setFillColor(sf::Color::White);
+    leftPaddle.setPosition(8, 80);
+
+    sf::RectangleShape rightPaddle;
+    rightPaddle.setSize(paddleSize);
+    rightPaddle.setFillColor(sf::Color::White);
+    rightPaddle.setPosition(152, 80);
+
+    sf::RectangleShape ball;
+    ball.setSize(ballSize);
+    ball.setFillColor(sf::Color::White);
+    ball.setPosition(80, 80);
+    sf::Vector2f ballVelocity(2, 1.6);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        ball.setPosition(ball.getPosition() + ballVelocity);
+
+        if (ball.getPosition().y > 156)
+        {
+            ballVelocity.y *= -1;
+        }
+        if (ball.getPosition().y < 0)
+        {
+            ballVelocity.y *= -1;
+        }
+        if (ball.getPosition().x > 158)
+        {
+            ballVelocity.x *= -1;
+        }
+        if (ball.getPosition().x < 0)
+        {
+            ballVelocity.x *= -1;
+        }
+
+        window.clear();
+        window.draw(leftPaddle);
+        window.draw(rightPaddle);
+        window.draw(ball);
+        window.display();
+        window.setFramerateLimit(60);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            window.close();
+        }
+    }
+}
+
 int main()
 {
     // videomode defines size of window
@@ -35,11 +100,13 @@ int main()
                     {
                     case 0:
                         // PUT FIRST OPTION ACTION HERE
-                        std::cout << "1st button pressed" << std::endl;
+                        std::cout << "PvP button pressed" << std::endl;
+                        pong();
+
                         break;
                     case 1:
                         // PUT SECOND OPTION ACTION HERE
-                        std::cout << "2nd button pressed" << std::endl;
+                        std::cout << "PvA button pressed" << std::endl;
                         break;
                     case 2:
                         window.close();
@@ -50,13 +117,14 @@ int main()
 
                 break;
 
-                
             case sf::Event::Closed:
                 window.close();
 
                 break;
-
-            case sf::Keyboard::Escape:
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            {
+                //if pong window is closed, 
                 window.close();
             }
         }
