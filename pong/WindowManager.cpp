c839d739734ac76a9fbcc940f105ebb623c22cf2
shadow <shadowcharmeleon@gmail.com>
2018-11-18 09:52:54 -0700

    game stops running when esc/window clsoed

--- pong/WindowManager.cpp
+++ pong/WindowManager.cpp
@@ -39,7 +39,14 @@ void WindowManager::Render() {
                                     while(Game.is_running)
                                     {
                                         Game.Loop();
+                                        if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+                                            windowContext.close();
+                                            break;
+                                        }
                                     }
+
+
+
                                     break;
                                 case 1:
                                     // std::cout << "VS AI pressed" << std::endl;
