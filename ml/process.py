e654351f43001e1def72028e140db160bbeb31d5
armiantos <sumitro@ualberta.ca>
2018-11-18 07:48:36 -0700

    fixed process.py indexing to the numpy array

--- ml/process.py
+++ ml/process.py
@@ -4,22 +4,22 @@ import numpy as np
 
 def getGameState():
     intermediate = []
+    result = []
+    count = 0
     pipein = os.open("/tmp/myFIFO", "r")
     line = pipein.readline();
     count = 0
-    m = 0
     for i in line:
-        if count < 80:
-            if i != 0:
-                intermediate[m].append(1)
-                count+=1
-            else:
-                intermediate[m].append(0)
-                count+=1
+        if i != str(0):
+            intermediate.append(1)
         else:
-            m+=1
-            count = 0
-    result = np.array(intermediate, np.int)
+            intermediate.append(0)
+        count += 1
+        if count == 80:
+        result.append(intermediate)
+        intermediate = []
+        count = 0
+    result = np.array(result, np.int)
     return result.astype(np.float).ravel()
 
 def sendGameInput(action):
